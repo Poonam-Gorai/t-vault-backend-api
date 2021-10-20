800604279dc16616cd8b3b9b2dab37f3fc2e9cf9
Poonam-Gorai <poonamgorai1807@gmail.com>
2021-10-20 14:48:16 +0530

    db changed

--- routes/route.js
+++ routes/route.js
@@ -20,7 +20,7 @@ router.post("/", async (req, res) => {
     owner: req.body.owner,
     type: req.body.type,
     description: req.body.description,
-    secrets: null,
+    secrets: []
   });
   try {
     const saveSafeList = await safeList.save();
